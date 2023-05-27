8cfe16774cefd8e393108b3d56b00ce1e55947aa
Benonking <benonkighoma@gmail.com>
2023-05-27 21:05:00 +0300

    status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import  Flask, jsonify
+from flask import  Flask
 from models import storage
 from api.v1.views import app_views
 
@@ -10,11 +10,11 @@ def cloz():
     '''display route / hello HBNB!'''
     storage.close()
 
-@app.errorhandler(404)
-def page_not_found():
-    '''
-    handler for 404 errors'''
-    return jsonify(error='Not found'), 404
+# @app.errorhandler(404)
+# def page_not_found():
+#     '''
+#     handler for 404 errors'''
+#     return jsonify(error='Not found'), 404
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port='5000', threaded=True)
