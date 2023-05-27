db9869a162b0e1b6d25e458bbe56bc2eea924b53
Benonking <benonkighoma@gmail.com>
2023-05-27 13:49:08 +0300

    handle page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import  Flask
+from flask import  Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -6,8 +6,15 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def Close():
+def cloz():
+    '''display route / hello HBNB!'''
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found():
+    '''
+    handler for 404 errors'''
+    return jsonify(error='Not found'), 404
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port='5000',threaded=True)
