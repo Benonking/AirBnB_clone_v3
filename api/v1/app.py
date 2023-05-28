036f7623d95390e24df73082a1d493330c14c272
Benonking <benonkighoma@gmail.com>
2023-05-28 10:02:28 +0300

    add page not found hamdler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
-from flask import  Flask
+from flask import  Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+import os, json
 HOST = os.environ.get('HBNB_API_HOST')
 PORT = os.environ.get('HBNB_API_PORT')
 
@@ -13,11 +13,12 @@ def cloz(exec):
     '''display route / hello HBNB!'''
     storage.close()
 
-# @app.errorhandler(404)
-# def page_not_found():
-#     '''
-#     handler for 404 errors'''
-#     return jsonify(error='Not found'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    '''
+     handler for 404 errors'''
+    #err = {'eror': 'Not found'}
+    return jsonify(error='Not found'), 404
 
 if __name__ == '__main__':
     if HOST and PORT:
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,14 @@
 from api.v1.views import app_views
+from flask import jsonify
 import json
 from models import city, place, amenity, state, user, storage
 import os
 STORAGE_TYPE = os.getenv('STORAGE_TYP')
 @app_views.route('/status', strict_slashes=False)
-def status():
+def status(exec):
     ''' returns a string that reports a ststus ok'''
-    dic = {'status': 'OK'}
-    return json.dumps(dic, indent=2)
+    #dic = {'status': 'OK'}
+    return jsonify(status='OK')
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
