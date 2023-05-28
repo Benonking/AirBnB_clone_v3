7cc52c3cf427f21c06e2df3cbc26a5df28980287
Benonking <benonkighoma@gmail.com>
2023-05-28 08:20:21 +0300

    status

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,4 +20,7 @@ def cloz():
 #     return jsonify(error='Not found'), 404
 
 if __name__ == '__main__':
-    app.run(debug = True, host=HOST, port=PORT, threaded=True)
+    if HOST and PORT:
+        app.run(host=HOST, port=PORT, debug=True, threaded=True)
+    else:
+        app.run(debug = True, host='0.0.0.0', port='5000', threaded=True)
